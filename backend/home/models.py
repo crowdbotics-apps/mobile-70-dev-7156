8d275e84f8f53e74945e96f1eac3e6872d6c7cdc
Crowdbotics <team@crowdbotics.com>
2020-07-07 06:17:29 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -9,6 +9,7 @@ from django.db import models
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
     nhghfhfh = models.CharField(max_length=256, null=True, blank=True,)
+    kjhkjhk = models.DurationField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
