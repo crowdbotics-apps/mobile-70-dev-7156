148f6eb26c5453664c94883c524a69c28b59ea53
Crowdbotics <team@crowdbotics.com>
2020-07-07 05:58:58 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen58142', human_name: 'Blank Screen 5', access_route: 'BlankScreen58142'},
 { name: 'BlankScreen48141', human_name: 'Blank Screen 4', access_route: 'BlankScreen48141'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
 { name: 'Calendar', human_name: 'Calendar', access_route: 'Calendar', icon: 'calendar'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen58142Navigator from '../features/BlankScreen58142/navigator';
 import BlankScreen48141Navigator from '../features/BlankScreen48141/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
 import CalendarNavigator from '../features/Calendar/navigator';
@@ -19,6 +20,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+BlankScreen58142: { screen: BlankScreen58142Navigator },
 BlankScreen48141: { screen: BlankScreen48141Navigator },
 Messenger: { screen: MessengerNavigator },
 Calendar: { screen: CalendarNavigator },
